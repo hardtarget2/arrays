a38aa64fe029205a297aa03eb42a5a1505ae9d7f
Ghost <bharry29@yahoo.com>
2020-03-14 20:37:29 -0400

    Completed 3/5 test cases.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//creating array and storing string
 var array = ["Banana", "Apples", "Oranges", "Blueberries"];
-array.splice(1);
+//removing index 0
+array.splice(0,1);
+//adding "Kiwi" to array
+array.push("Kiwi");
+//sorting the array 
+array.sort();
+//printing the contient of the array
 console.log(array);
+///////
